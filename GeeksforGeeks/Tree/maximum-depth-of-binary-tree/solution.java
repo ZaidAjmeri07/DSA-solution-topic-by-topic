5b6179ceb448c40b9647ca43c8cdfaca4df717f7
Zaid Ajmeri <zaidajmeri201@gmail.com>
2026-09-25 10:50:31 +0530

    Pushed via PUSHkar

---
+++ GeeksforGeeks/Tree/maximum-depth-of-binary-tree/solution.java
@@ -0,0 +1,22 @@
+/*  Binary Tree Node Structure
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+class Solution {
+    public static int maxDepth(Node root) {
+        // code here
+        
+        if(root == null) return 0;
+
+               return 1 + Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+}
